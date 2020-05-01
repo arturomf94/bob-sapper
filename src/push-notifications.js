ce224a857f5fe03056f7bb1bc3f2036dbfb1ca54
Arturo Marquez <arturomf94@gmail.com>
2020-05-01 16:13:16 -0500

    Register service worker and add user subscription

--- src/push-notifications.js
+++ src/push-notifications.js
@@ -12,7 +12,44 @@ async function askUserPermission() {
   return await Notification.requestPermission();
 }
 
+/**
+ *
+ */
+function registerServiceWorker() {
+  return navigator.serviceWorker.register("/service-worker.js");
+}
+
+/**
+ *
+ * using the registered service worker creates a push notification subscription and returns it
+ *
+ */
+async function createNotificationSubscription() {
+  //wait for service worker installation to be ready
+  const serviceWorker = await navigator.serviceWorker.ready;
+  // subscribe and return the subscription
+  return await serviceWorker.pushManager.subscribe({
+    userVisibleOnly: true,
+    applicationServerKey: pushServerPublicKey
+  });
+}
+
+/**
+ * returns the subscription if present or nothing
+ */
+function getUserSubscription() {
+  //wait for service worker installation to be ready, and then
+  return navigator.serviceWorker.ready
+    .then(function(serviceWorker) {
+      return serviceWorker.pushManager.getSubscription();
+    })
+    .then(function(pushSubscription) {
+      return pushSubscription;
+    });
+}
+
 export {
   isPushNotificationSupported,
   askUserPermission,
+  registerServiceWorker,
 };
--- src/service-worker.js
+++ src/service-worker.js
@@ -7,6 +7,34 @@ const ASSETS = `cache${timestamp}`;
 const to_cache = shell.concat(files);
 const cached = new Set(to_cache);
 
+function receivePushNotification(event) {
+  console.log("[Service Worker] Push Received.");
+
+  const { image, tag, url, title, text } = event.data.json();
+
+  const options = {
+    data: url,
+    body: text,
+    icon: image,
+    vibrate: [200, 100, 200],
+    tag: tag,
+    image: image,
+    badge: "https://spyna.it/icons/favicon.ico",
+    actions: [{ action: "Detail", title: "View", icon: "https://via.placeholder.com/128/ff0000" }]
+  };
+  event.waitUntil(self.registration.showNotification(title, options));
+}
+
+function openPushNotification(event) {
+  console.log("[Service Worker] Notification click Received.", event.notification.data);
+
+  event.notification.close();
+  event.waitUntil(clients.openWindow(event.notification.data));
+}
+
+self.addEventListener("push", receivePushNotification);
+self.addEventListener("notificationclick", openPushNotification);
+
 self.addEventListener('install', event => {
 	event.waitUntil(
 		caches
